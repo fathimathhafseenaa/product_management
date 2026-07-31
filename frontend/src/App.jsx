a7f317389bf13abd5e1b2a87ec675ad2096d5bce
fathimathhafseena <fathimathhafseenaa@gmail.com>
2026-07-31 13:48:59 +0530

    updated frontend changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
@@ -9,34 +9,54 @@ import AddCategory from "./pages/AddCategory/AddCategory";
 import AddSubCategory from "./pages/AddSubCategory/AddSubCategory";
 import AddProduct from "./pages/Addproduct/AddProduct";
 
+function ProtectedRoute({ children }) {
+  const token = localStorage.getItem("token");
+
+  if (!token) {
+    return <Navigate to="/" replace />;
+  }
+
+  return children;
+}
+
+function PublicRoute({ children }) {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    return <Navigate to="/home" replace />;
+  }
+
+  return children;
+}
+
 function App() {
   return (
   
       <Routes>
 
         {/* Login */}
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<PublicRoute><Login /></PublicRoute>} />
 
         {/* Signup */}
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/signup" element={<PublicRoute><Signup /></PublicRoute>} />
 
         {/* Home */}
-        <Route path="/home" element={<Home />} />
+        <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
 
         {/* Product Details */}
-        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/product/:id" element={<ProtectedRoute><ProductDetails /></ProtectedRoute>} />
 
         {/* Wishlist */}
-        <Route path="/wishlist" element={<Wishlist />} />
+        <Route path="/wishlist" element={<ProtectedRoute><Wishlist /></ProtectedRoute>} />
 
         {/* Add Category */}
-        <Route path="/add-category" element={<AddCategory />} />
+        <Route path="/add-category" element={<ProtectedRoute><AddCategory /></ProtectedRoute>} />
 
         {/* Add Sub Category */}
-        <Route path="/add-subcategory" element={<AddSubCategory />} />
+        <Route path="/add-subcategory" element={<ProtectedRoute><AddSubCategory /></ProtectedRoute>} />
 
         {/* Add Product */}
-        <Route path="/add-product" element={<AddProduct />} />
+        <Route path="/add-product" element={<ProtectedRoute><AddProduct /></ProtectedRoute>} />
 
       </Routes>
 
